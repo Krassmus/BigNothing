7cf2a54a44794630172af34fcdbc1ed6b0c89aff
Rasmus Fuhse <krassmus@gmail.com>
2019-01-10 08:02:37 +0100

    work on

--- modules/authentication/Authentication.php
+++ modules/authentication/Authentication.php
@@ -20,7 +20,30 @@ class Authentication extends \Module
     public static function authenticateUser($hook)
     {
         if ($hook->getLogin() && $hook->getPassword()) {
-            $hook->authenticateLogin(true);
+            $db = \DBManager::getInstance();
+            $statement = $db->prepare("
+                SELECT * 
+                FROM users 
+                WHERE login = :login
+            ");
+            $statement->execute(array('login' => $hook->getLogin()));
+            $data = $statement->fetch(PDO::FETCH_ASSOC);
+            if ($data['password_hash'] === hashPassword($hook->getPassword())) {
+                $hook->authenticateLogin(true);
+            } elseif($data['password_hash'] === md5($hook->getPassword())) {
+                $hook->authenticateLogin(true);
+                //change md5-password
+                $newpassword = hashPassword($hook->getPassword());
+                $statement = $db->prepare("
+                    UPDATE users
+                    SET password_hash = :newpassword 
+                    WHERE login = :login
+                ");
+                $statement->execute(array(
+                    'login' => $hook->getLogin(),
+                    'newpassword' => $newpassword
+                ));
+            }
         }
     }
 }
